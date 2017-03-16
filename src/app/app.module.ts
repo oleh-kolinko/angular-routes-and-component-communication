5e01e17d2f5a38e1ba941b21f73168bf9234f441
Oleh Kolinko <oleh.kolinko@gmail.com>
2017-03-16 15:48:34 -0400

    routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,27 +2,42 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { Routes } from '@angular/router';
+import { Routes , RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { QuoteItemComponent } from './quote-item/quote-item.component';
 import { QuoteListComponent } from './quote-list/quote-list.component';
+import { MyHomeComponent } from './my-home/my-home.component';
+import { MyAboutComponent } from './my-about/my-about.component';
+import { ContactListComponent } from './contact-list/contact-list.component';
+import { ContactComponent } from './contact/contact.component';
 
 const routes: Routes = [
-  //http://localhost:4200/quotes
-  { path: 'quotes', component: QuoteListComponent }
+
+  { path: 'quotes', component: QuoteListComponent },//http://localhost:4200/quotes
+  { path: 'home', component: MyHomeComponent },
+  { path: 'about', component: MyAboutComponent },
+  { path: 'contacts', component: ContactListComponent },
+  { path: 'contact/:id', component: ContactComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+
 ]
 
 @NgModule({
   declarations: [
     AppComponent,
     QuoteItemComponent,
-    QuoteListComponent
+    QuoteListComponent,
+    MyHomeComponent,
+    MyAboutComponent,
+    ContactListComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
